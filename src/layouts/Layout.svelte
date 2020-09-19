0287c2224e013ff41fb98f0f0dcc91ffd3106cad
kevmodrome <kevin@kevmodro.me>
2020-09-19 18:00:47 +0200

    preloads fonts

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,9 +1,36 @@
 <script>
   export let routeHTML, settings;
-  const assetDir = settings.locations.assets.replace(settings.locations.public, '/');
+  const assetDir = settings.locations.assets.replace(
+    settings.locations.public,
+    "/"
+  );
 </script>
 
 <svelte:head>
+  <link
+    rel="preload"
+    href="/dist/static/fonts/Anton-Regular.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin />
+  <link
+    rel="preload"
+    href="/dist/static/fonts/Overpass-Regular.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin />
+  <link
+    rel="preload"
+    href="/dist/static/fonts/Overpass-Bold.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin />
+  <link
+    rel="preload"
+    href="/dist/static/fonts/Inter-Regular.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin />
   <link rel="stylesheet" href="{assetDir}style.css" />
 </svelte:head>
 <div class="container">
