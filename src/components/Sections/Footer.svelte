3493c977baa1c8e8172ea34dd00c1e5d8225d42d
kevmodrome <kevin@kevmodro.me>
2020-09-21 21:47:48 +0200

    adds logos for svelte school and mono

--- src/components/Sections/Footer.svelte
+++ src/components/Sections/Footer.svelte
@@ -1,19 +1,22 @@
 <style>
   .container {
+    color: #808080;
     width: calc(100% - 50px);
     --small-max-width: var(--media-lte-sm) 300px;
     max-width: var(--small-max-width, var(--container-width));
     margin: 0 auto;
-    opacity: 0.5;
-    --small-padding: var(--media-lte-sm) 40px 25px 130px 25px;
-    padding: var(--small-padding, 80px 25px 200px 25px);
+    --small-padding: var(--media-lte-sm) 40px 25px 110px 25px;
+    padding: var(--small-padding, 80px 25px 150px 25px);
 
     display: grid;
+    grid-gap: 30px;
     --small-grid: var(--media-lte-sm) auto;
-    grid-template-columns: var(--small-grid, auto auto);
+    grid-template-columns: var(--small-grid, auto auto auto);
 
     --small-justify: var(--media-lte-sm) center;
     justify-content: var(--small-justify, space-between);
+
+    align-items: center;
 
     --text-center: var(--media-lte-sm) center;
     text-align: var(--text-center, initial);
@@ -21,30 +24,43 @@
     line-height: 200%;
   }
 
+  .organised-by {
+    display: grid;
+    place-items: center;
+    grid-gap: 10px;
+    grid-template-columns: 1fr;
+  }
+  .organised-by img {
+    height: 60px;
+  }
+
   .design-by {
     display: grid;
-    --small-justify: var(--media-lte-sm) center;
-    justify-content: var(--small-justify, end);
+    place-items: center;
+    grid-gap: 10px;
+    grid-template-columns: 1fr;
+  }
+
+  .design-by img {
+    height: 30px;
   }
 </style>
 
 <div class="container">
   <div class="info">Svelte Summit is a volunteer effort.</div>
+  <div class="organised-by">
+    <span>Organized by</span><a target="_blank" rel="noreferrer" href="https://svelte.school"><img
+        src="dist/static/images/svelteschool.svg"
+        alt="Svelte School Logo" /></a>
+  </div>
   <div class="design-by">
-    <span>
-      Design by
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://twitter.com/mono_company">
-        @mono_company
-      </a>
-    </span>
-    <span>
-      Built by
-      <a target="_blank" rel="noreferrer" href="https://svelte.school">
-        Svelte School
-      </a>
-    </span>
+    <span> Design by </span>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://twitter.com/mono_company"><img
+        src="dist/static/images/mono.svg"
+        alt="mono logo" />
+    </a>
   </div>
 </div>
