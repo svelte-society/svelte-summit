9cb8aea9f7c28d9bf550cfa0cf009a146326b562
kevmodrome <kevin@kevmodro.me>
2020-09-22 19:02:09 +0200

    add canonical link

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -10,6 +10,7 @@
 
 <svelte:head>
   <title>Svelte Summit 2020</title>
+  <link rel="canonical" href="https://sveltesummit.com/" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!-- Primary Meta Tags -->
   <title>Svelte Summit is a FREE whole-day online event.</title>
