b823d0630d22c6759f604e9c6ee75056fbc8b1a1
sw-yx <shawnthe1@gmail.com>
2020-10-04 08:22:32 +0800

    add favicon

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -28,6 +28,8 @@
     type="font/woff2"
     crossorigin /> -->
   <link rel="stylesheet" href="/style.css" />
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏔️</text></svg>">
+
   <!-- <link rel="stylesheet" href="/fonts/fonts.css" /> -->
 </svelte:head>
 <div class="container">
