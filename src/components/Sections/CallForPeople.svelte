b40c2800574101f88b04a4c28d4376e1011604a6
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-30 19:06:18 +0200

    remove CFP CTA

--- src/components/Sections/CallForPeople.svelte
+++ src/components/Sections/CallForPeople.svelte
@@ -1,63 +1,50 @@
 <script>
-  import Card from '../Card.svelte';
-  import Button from '../Button.svelte';
+	import Card from "../Card.svelte";
+	import Button from "../Button.svelte";
 </script>
 
-<style>
-  .background {
-    padding: 100px 0;
-    
-    background: var(--background-super-dark);
-    display: grid;
-    justify-content: center;
-  }
-  .container {
-    display: grid;
-    --small-grid: var(--media-lte-sm) 0;
-    grid-gap: var(--small-grid, 30px);
-    max-width: var(--container-width);
-  }
-  h3 {
-    font-size: 28px;
-    font-weight: normal;
-  }
-  p {
-    font-size: 16px;
-    --small-font-size: var(--media-lte-sm) 14px;
-  }
-  h3,
-  p {
-    margin: 0;
-    --small-width: var(--media-lte-sm) 100%;
-    width: var(--small-width, 90%);
-    color: var(--sky-blue);
-  }
-</style>
-
 <div class="background" id="cfp">
-  <div class="container">
-    <Card center>
-      <h3>Call for initial proposals - March 21st, 2021!</h3>
-      <p>
-        If you're interested in doing a talk, the button below will bring you to the right place! First time doing a
-        talk? No Problem! We welcome talks of all levels, from beginner to advanced, long and short. If you have more
-        specific questions you can reach us on the <a
-          rel="noreferrer"
-          target="_blank"
-          href="https://discord.gg/qzfM2JU">
-          Svelte discord server
-        </a> .
-      </p>
-      <Button href="https://sessionize.com/svelte-summit-spring-2021/">Send proposal</Button>
-    </Card>
-    <Card center>
-      <h3>Looking for sponsors</h3>
-      <p>
-        Since last time we've decided to try to bring the online experience to another level by improving the quality.
-        If you (or your company) are interested in sponsoring the event, reach out to us via email using the link below
-        and we can have a chat.
-      </p>
-      <Button href="mailto:sponsors@sveltesociety.dev" black>Send email</Button>
-    </Card>
-  </div>
+	<div class="container">
+		<Card center>
+			<h3>Looking for sponsors</h3>
+			<p>
+				Since last time we've decided to try to bring the online experience to
+				another level by improving the quality. If you (or your company) are
+				interested in sponsoring the event, reach out to us via email using the
+				link below and we can have a chat.
+			</p>
+			<Button href="mailto:sponsors@sveltesociety.dev" black>Send email</Button>
+		</Card>
+	</div>
 </div>
+
+<style>
+	.background {
+		padding: 100px 0;
+
+		background: var(--background-super-dark);
+		display: grid;
+		justify-content: center;
+	}
+	.container {
+		display: grid;
+		--small-grid: var(--media-lte-sm) 0;
+		grid-gap: var(--small-grid, 30px);
+		max-width: var(--container-width);
+	}
+	h3 {
+		font-size: 28px;
+		font-weight: normal;
+	}
+	p {
+		font-size: 16px;
+		--small-font-size: var(--media-lte-sm) 14px;
+	}
+	h3,
+	p {
+		margin: 0;
+		--small-width: var(--media-lte-sm) 100%;
+		width: var(--small-width, 90%);
+		color: var(--sky-blue);
+	}
+</style>
